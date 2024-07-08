683e9fe96fa4d64390d415dc0916c3198657c710
Priyanka K. Sundalam <ksundalam@gmail.com>
2024-07-09 01:58:23 +0530

    created login user route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken')
 const router = express.Router()
 const User = require('../models/user.model.js')
 
-//create a user using POST : /api/auth - no need of a logged in user 
+//create a user using POST : /api/auth/createuser - no need of a logged in user 
 router.post('/createuser',
     //express-validator 
     [
@@ -15,7 +15,7 @@ router.post('/createuser',
         body('password', 'password must be atleast 5 characters long').isLength({ min: 5 })
     ],
     async (req, res) => {
-
+        // validate req.body data
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() })
@@ -40,16 +40,58 @@ router.post('/createuser',
 
             const data = {
                 user: {
-                    id : user._id
+                    id: user._id
                 }
             }
-            const authToken = jwt.sign(data, "shhhh")
+            const authToken = jwt.sign(data, process.env.JWT_SECRET)
 
             //return res
             // res.json(user)
-            res.json({authToken:authToken})
+            res.json({ authToken: authToken })
 
         } catch (error) {
+            console.log(error.message)
+            return res.status(500).json({ error: error.message })
+        }
+    }
+)
+
+//login user - POST : /api/auth/login - no need of logged in user
+router.post('/login',
+    //express-validator 
+    [
+        body('email', 'enter a valid email').isEmail(),
+        body('password', 'password cant be blank').exists()
+    ],
+    async (req, res) => {
+        // validate req.body data
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        const {email, password} = req.body;
+        try{
+            let user = await User.findOne({email})
+            if(!user){
+                res.status(400).json({error: 'Please try to login with correct credentials'})
+            }
+
+            const passwordCompare = await bcrypt.compare(password, user.password)
+            if(!passwordCompare){
+                return res.status(400).json({error: 'Please try to login with correct credentials'})
+            }
+
+            //if user is with correct details
+            const data = {
+                user: {
+                    id: user._id
+                }
+            }
+            const authToken = jwt.sign(data, process.env.JWT_SECRET)
+
+            res.json({ authToken: authToken })
+        }catch(error){
             console.log(error.message)
             return res.status(500).json({ error: error.message })
         }
