7cfb630babbf16d387f33613eccec65d9cb80a3c
Priyanka K. Sundalam <ksundalam@gmail.com>
2024-07-09 00:47:00 +0530

    used express-validator

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,16 +1,32 @@
 const express = require('express')
+const { body, validationResult } = require('express-validator');
 const router = express.Router()
 const User = require('../models/user.model.js')
 
 //create a user using POST : /api/auth
 
-router.post('/', async (req, res)=>{
-    const result = await User.create(req.body)
+router.post('/',
+    [
+        body('name', 'enter a valid name').isLength({min: 3}),
+        body('email', 'enter a valid email').isEmail(),
+        body('password', 'password must be atleast 5 characters long').isLength({min : 5})
+    ] ,
+    async (req, res)=>{
 
-    if(!result)
-        throw new Error("Failure to insert")
-    console.log(req.body);
-    res.json(req.body)
-})
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        }).then(user => res.json(user))
+        .catch(error => {console.log(error)
+            res.json({error: error.message})
+        })
+    }
+)
 
 module.exports = router
