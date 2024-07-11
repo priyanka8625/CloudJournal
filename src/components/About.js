8b1f6785c78d0402114e7b617a9dc3c5b2532bcb
Priyanka K. Sundalam <ksundalam@gmail.com>
2024-07-12 01:36:25 +0530

    useLocation added

--- src/components/About.js
+++ src/components/About.js
@@ -3,16 +3,9 @@ import { useContext, useEffect } from 'react'
 import NoteContext from '../context/notes/NoteContext.js'
 
 const About = () => {
-  //using states from react Context API 
-  const a = useContext(NoteContext)
-  useEffect(() => {
-    a.update()
-    // eslint-disable-next-line
-  }, [])//pass empty [] to run useEffect only 1 time after component is mount
-  
   return (
     <div>
-      About {a.state.name} and his class is {a.state.class}
+      About 
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,23 +2,9 @@ import { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props)=>{
-    const stateValues = {
-        "name": "Priyanka",
-        "class": "S1"
-    }
-    //creating state and updating them utility
-    const [state, setState] = useState(stateValues)
-
-    const update = ()=>{
-        setTimeout(() => {
-            setState({
-                "name": state.name+"-Shreyash",
-                "class": state.class+"-S2"
-            })
-        }, 1000);
-    }
+    
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
