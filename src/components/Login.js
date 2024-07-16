8d1d85a6336c69e60ef1848facc768a9f98b89de
Priyanka K. Sundalam <ksundalam@gmail.com>
2024-07-17 00:54:24 +0530

    added signup component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,17 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const [credentials, setCredentials] = useState({email:"", password:""});
+    const [credentials, setCredentials] = useState({ email: "", password: "" });
     let navigate = useNavigate()
-    
-    const handleSubmit = async (e)=>{
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const url = "http://localhost:5000/api/auth/login";
 
         const response = await fetch(url, {
             method: "POST",
-            headers: { 
+            headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
@@ -20,30 +20,32 @@ const Login = () => {
             })
         });
         const json = await response.json();
-        if(json.success){
+        if (json.success) {
             //save auth token and redirec to '/'
             localStorage.setItem('token', json.authToken)
             navigate('/')
-        }else{
+        } else {
             alert("Invaid credentials")
         }
     }
-    const onChange = (e)=>{
-        setCredentials({...credentials, [e.target.name]:e.target.value})
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-                <label htmlFor="email" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="email" name='email' value={credentials.email} onChange={onChange} aria-describedby="emailHelp"/>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" name='email' value={credentials.email} onChange={onChange} aria-describedby="emailHelp" />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" className="form-control" id="password" value={credentials.password} onChange={onChange} name='password'/>
-            </div>
-            <button type="submit" className="btn btn-primary" >Submit</button>
-        </form>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" value={credentials.password} onChange={onChange} name='password' />
+                </div>
+                <button type="submit" className="btn btn-primary" >Submit</button>
+            </form>
+        </div>
     )
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-  return (
-    <div>
-      Signup
-    </div>
-  )
+    const [credentials, setCredentials] = useState({ email: "", password: "" });
+    let navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const url = "http://localhost:5000/api/auth/createuser";
+        const {name, email, password} = credentials
+
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })
+        });
+        const json = await response.json();
+        console.log(json);
+        if (json.success) {
+            //save auth token and redirec to '/'
+            localStorage.setItem('token', json.authToken)
+            navigate('/')
+        } else {
+            alert("Invalid credentials")
+        }
+    }
+
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
+    return (
+        <div className='container' >
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="name" name='name' onChange={onChange} aria-describedby="emailHelp" required minLength={3}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" name='email' onChange={onChange} aria-describedby="emailHelp" required/>
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" name='password' onChange={onChange} required minLength={5}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+                    <input type="password" className="form-control" id="cpassword" name='cpassword' onChange={onChange} required minLength={5}/>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default Signup
